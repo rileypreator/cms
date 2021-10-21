715a9781b695ba0e4223ed789c57756704d99ed5
rileypreator <pre15001@byui.edu>
2021-10-20 18:06:21 -0600

    Contact details routing added

--- src/app/contacts/contact-list/contact-item/contact-item.component.ts
+++ src/app/contacts/contact-list/contact-item/contact-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Contact } from '../../contact.model';
 
 @Component({
   selector: 'app-contact-item',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-item.component.css']
 })
 export class ContactItemComponent implements OnInit {
+  @Input() contact: Contact;
 
   constructor() { }
 
--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Contact } from '../contact.model';
 
 @Component({
@@ -12,6 +12,13 @@ export class ContactListComponent implements OnInit {
     new Contact("1", "R. Kent Jackson", "jacksonk@byui.edu", "208-496-3771", "../../assets/images/jacksonk.jpg", "null"),
     new Contact("2", "Rex Barzee", "barzeer@byui.edu", "208-496-3768", "../../assets/images/barzeer.jpg", "null")
   ];
+
+  //Used to pass output data to the contacts then down to contact-detail
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
+
+  onSelected(contact: Contact) {
+    this.selectedContactEvent.emit(contact);
+  }
 
   constructor() { }
 
