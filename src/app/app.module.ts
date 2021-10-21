dd3ad1bd45e66700a86292b89360b281dbf4d606
rileypreator <pre15001@byui.edu>
2021-10-20 18:54:52 -0600

    Added document components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 import { ContactItemComponent } from './contacts/contact-list/contact-item/contact-item.component';
+import { DocumentListComponent } from './documents/document-list/document-list.component';
+import { DocumentItemComponent } from './documents/document-list/document-item/document-item.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { DocumentsComponent } from './documents/documents.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { ContactItemComponent } from './contacts/contact-list/contact-item/conta
     ContactsComponent,
     ContactListComponent,
     ContactDetailComponent,
-    ContactItemComponent
+    ContactItemComponent,
+    DocumentListComponent,
+    DocumentItemComponent,
+    DocumentDetailComponent,
+    DocumentsComponent
   ],
   imports: [
     BrowserModule
