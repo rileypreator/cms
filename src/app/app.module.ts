8afca5470b5abe39dae2fdcb386a63600d5d38dc
rileypreator <60445429+rileypreator@users.noreply.github.com>
2021-11-21 22:12:50 -0700

    Added functionality to Document Edit Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 import { ContactEditComponent } from './contacts/contact-edit/contact-edit.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,8 @@ import { ContactEditComponent } from './contacts/contact-edit/contact-edit.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-document-edit',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentEditComponent implements OnInit {
 
-  constructor() { }
+  //variables for the document changes that can be made
+  originalDocument: Document;
+  document: Document;
+  editMode: boolean = false;
+
+  constructor(private documentService: DocumentService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      let id = params["id"];
+
+      if (!id) {
+        this.editMode = false;
+        return;
+      }
+
+      this.originalDocument = this.documentService.getDocument(id);
+
+      if (!this.originalDocument) {
+        return;
+      }
+
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    let value = form.value;
+    let newDocument = new Document("", value.name, value.description, value.url, null);
+
+    if (this.editMode == true) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    }
+    else {
+      this.documentService.addDocument(newDocument);
+    };
+
+    this.router.navigate(['/documents'])
+  }
+
+  onCancel() {
+    this.router.navigate(['/documents']);
   }
 
 }
