378a8732f898632ef2beb87bcfef8d8c4c8f4700
rileypreator <60445429+rileypreator@users.noreply.github.com>
2021-11-21 23:51:16 -0700

    Added drag and drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 import { ContactEditComponent } from './contacts/contact-edit/contact-edit.component';
 import { FormsModule } from '@angular/forms';
-
+import { DndModule } from 'ng2-dnd';
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,7 +41,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    DndModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -38,11 +38,12 @@ export class ContactEditComponent implements OnInit {
       this.contact = JSON.parse(JSON.stringify(this.originalContact));
 
       if (this.contact.group) {
-        this.groupContacts = JSON.parse(JSON.stringify(this.groupContacts));
+        this.groupContacts = JSON.parse(JSON.stringify(this.contact.group));
       }
     });
   }
 
+  //this will submit the form to edit or add a number contact
   onSubmit(form: NgForm) {
     let value = form.value;
     let newContact = new Contact("", value.name, value.email, value.phone, value.imageUrl, this.groupContacts);
@@ -57,8 +58,50 @@ export class ContactEditComponent implements OnInit {
     this.router.navigate(['/contacts']);
   }
 
+  //This will return to the contacts page if and edit was being made but was cancelled
   onCancel() {
     this.router.navigate(['/contacts']);
+  }
+
+  isInvalidContact(newContact: Contact) {
+    if (!newContact) {
+      return true;
+    }
+
+    if (this.contact && newContact.id === this.contact.id) {
+      return true;
+    }
+
+    for (let i = 0; i < this.groupContacts.length; i++) {
+      if (newContact.id === this.groupContacts[i].id) {
+        return true;
+      }
+    }
+
+    return false;
+
+  }
+
+  addToGroup($event: any) {
+    console.log("Added to group;")
+    const selectedContact: Contact = $event.dragData;
+    const invalidGroupContact = this.isInvalidContact(selectedContact);
+
+    if (invalidGroupContact) {
+      console.log("Is invalid contact")
+      return;
+    }
+
+    this.groupContacts.push(selectedContact);
+    console.log(selectedContact);
+  }
+
+  onRemoveItem(index: number) {
+    if (index < 0 || index >= this.groupContacts.length) {
+      return;
+    }
+
+    this.groupContacts.splice(index, 1);
   }
 
 }
