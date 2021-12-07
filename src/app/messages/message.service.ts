0f183e8bb07f6186af70680ca616fc138559daf7
rileypreator <60445429+rileypreator@users.noreply.github.com>
2021-12-07 00:07:00 -0700

    Added Firebase support for Messages

--- src/app/messages/message.service.ts
+++ src/app/messages/message.service.ts
@@ -1,16 +1,16 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Message } from './message.model';
 import { MOCKMESSAGES } from './MOCKMESSAGES';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
   messages: Message[] = [];
-
+  maxMessageId: number;
   messageChangedEvent = new EventEmitter<Message[]>();
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this.messages = MOCKMESSAGES;
    }
 
@@ -18,15 +18,58 @@ export class MessageService {
   getMessage(id: string): Message {
     return this.messages.find((message) => message.id === id);
   }
-  // gets the entire array of messages
-  getMessages() {
-    return this.messages.slice();
-  }
 
   //adds a message to the array
   addMessage(message: Message) {
     console.log("Add message", message)
     this.messages.push(message);
-    this.messageChangedEvent.emit(this.messages.slice());
+    this.storeMessages();
+  }
+
+  //gets the max ID that will be added to the newest message
+  getMaxID(): number {
+    let maxId = 0;
+
+    this.messages.forEach((message) => {
+      let currentId = parseInt(message.id);
+
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    });
+
+    return maxId;
+  }
+
+  //gets the messages from Firebase
+  getMessages() {
+    this.httpClient.get("https://cms-project-abe2d-default-rtdb.firebaseio.com/messages.json")
+
+    //success method
+      .subscribe((messages: Message[]) => {
+        this.messages = messages;
+        this.maxMessageId = this.getMaxID();
+        this.messages.sort((a, b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0));
+        this.messageChangedEvent.next(this.messages.slice());
+      },
+      
+      (error: any) => {
+        console.log(error);
+      }
+      );
+
+      return this.messages;
+  }
+
+  storeMessages() {
+    let JSONmessages = JSON.stringify(this.messages);
+
+    let headers = new HttpHeaders({'Content-Type': 'application/json'});
+    
+    this.httpClient.put("https://cms-project-abe2d-default-rtdb.firebaseio.com/messages.json", JSONmessages,{headers: headers})
+      .subscribe(() => {
+        this.messageChangedEvent.next(this.messages.slice())
+      }
+      )
   }
 }
